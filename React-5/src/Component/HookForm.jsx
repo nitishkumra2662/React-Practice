591ef77a7ef0657e03b55dfd78c418682b0766e1
Nitish Kumra <nitishkumra2662@gmail.com>
2025-06-20 22:33:55 +0530

    Update-Button

--- React-5/src/Component/HookForm.jsx
+++ React-5/src/Component/HookForm.jsx
@@ -4,47 +4,47 @@ import { useNavigate } from 'react-router';
 
 
 const HookForm = () => {
-    const navigate=useNavigate();
+    const navigate = useNavigate();
 
     const {
         register,
         handleSubmit,
         watch,
-        formState: { errors,isValid,isDirty },
-    } = useForm({defaultValues:{Age:20}});
+        formState: { errors, isValid, isDirty },
+    } = useForm({ defaultValues: { Age: 20 } });
     console.log(errors);
-    const firstName=watch("Age")
-console.log(firstName);
+    const firstName = watch("Age")
+    console.log(firstName);
     function onSubmit(data) {
         console.log("Form was Submitted", data);
     }
-    function handelBack(){
-navigate(-1)
+    function handelBack() {
+        navigate(-1)
     }
     return (
         <>
-        <button onClick={handelBack}>
-            Go To Back
+            <button onClick={handelBack}>
+                Go To Back
 
-        </button>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label> First Name:</label>
-                <input {...register("FirstName", { required: "first Name is required" })} />
-                {errors?.FirstName?.message}
-            </div>
-            <br />
-            <div>
-                <label> Last Name:</label>
-                <input {...register("LastName", { required: true })} />
-            </div>
-            <br />
-            <div>
-                <label> Age:</label>
-                <input {...register("Age", { required: true })} />
-            </div>
-            <input disabled={!isDirty||!isValid}type="submit" />
-        </form>
+            </button>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label> First Name:</label>
+                    <input {...register("FirstName", { required: "first Name is required" })} />
+                    {errors?.FirstName?.message}
+                </div>
+                <br />
+                <div>
+                    <label> Last Name:</label>
+                    <input {...register("LastName", { required: true })} />
+                </div>
+                <br />
+                <div>
+                    <label> Age:</label>
+                    <input {...register("Age", { required: true })} />
+                </div>
+                <input disabled={!isDirty || !isValid} type="submit" />
+            </form>
         </>
     )
 }
